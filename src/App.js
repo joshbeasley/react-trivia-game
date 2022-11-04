8fcc4515f00c001302087280e703723ea0a96863
Josh Beasley <josh.beasley@hotmail.com>
2022-11-04 09:40:57 -0700

    added file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Leaderboard from './components/Leaderboard/Leaderboard';
+import Nav from './components/Nav/Nav'
+import Home from './components/Home/Home';
+import Game from './components/Game/Game';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Routes >
+        <Route path='/' element={<Home/>}/>
+        <Route path='/game' element={<Game/>}/>
+        <Route path='/leaderboard' element={<Leaderboard/>}/>
+      </Routes>
     </div>
   );
 }
